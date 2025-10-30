a7ad4579aaffb06a76efbe103246f5108b3a0243
mrodriguez-sqdm <manuelrodriguez@sqdm.com>
2025-10-30 14:04:33 -0500

    Version actual de capas para INI4147

---
+++ etb-utilities-connector/src/main/java/org/mule/extension/etbutilities/internal/EtbUtilitiesExtension.java
@@ -0,0 +1,16 @@
+package org.mule.extension.etbutilities.internal;
+import org.mule.runtime.extension.api.annotation.Extension;
+import org.mule.runtime.extension.api.annotation.Configurations;
+import org.mule.runtime.extension.api.annotation.dsl.xml.Xml;
+
+
+/**
+ * This is the main class of an extension, is the entry point from which configurations, connection providers, operations
+ * and sources are going to be declared.
+ */
+@Xml(prefix = "etbutilities")
+@Extension(name = "EtbUtilities")
+@Configurations(EtbUtilitiesConfiguration.class)
+public class EtbUtilitiesExtension {
+
+}
